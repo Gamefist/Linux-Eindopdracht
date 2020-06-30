91c9a4b1379bd4f203e183de70d87fd076de0b4b
Gamefist <ikheetdennis147@gmail.com>
2020-06-30 21:20:49 +0200

    Fixed installation parameters

--- docker.sh
+++ docker.sh
@@ -3,12 +3,12 @@
 if ! dpkg -l docker-ce;
 then
 
-sudo apt-get update
+sudo apt-get update -y
 sudo sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
+sudo apt update -y
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt-get install -y docker-ce
 
 fi 
