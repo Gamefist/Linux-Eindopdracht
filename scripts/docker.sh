88a044ae717fd7caea6a7376cdaf270be465031b
Gamefist <ikheetdennis147@gmail.com>
2020-06-30 23:11:17 +0200

    Changed command details

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,8 +7,8 @@ sudo apt-get update -y
 sudo sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update -y
+sudo apt-get update -y
 apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
 
-fi 
+fi
