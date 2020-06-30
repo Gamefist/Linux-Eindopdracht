49de6448b8410be5122f43f123a8002305e802a0
Gamefist <ikheetdennis147@gmail.com>
2020-06-30 15:36:47 +0200

    Update installsalt.sh
    Script now actually works

--- installsalt.sh
+++ installsalt.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-curl -L https://bootstrap.saltstack.com -o install_salt.sh
-
-sudo sh install_salt.sh -A 10.0.6.141
-
-service salt-minion restart
-
-echo "Minion installed succesfully
+if ! command -v salt;
+  then
+    curl -L https://bootstrap.saltstack.com -o install_salt.sh
+    sudo sh install_salt.sh
+    if ! grep -Fxq "master: 10.0.6.141" "/etc/salt/minion";
+      then
+        echo "master: 10.0.6.141" >> /etc/salt/minion
+        echo "id: Wordpress" >> /etc/salt/minion
+        service salt-minion restart
+        echo "Minion installed succesfully"
+    fi
+else
+  echo "Minion is already installed"
+fi
